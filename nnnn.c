1bf52336701cbda47968be318b1571626bab23e9
kumaransabapathy <31533930+kumaransabapathy@users.noreply.github.com>
2018-01-24 16:17:32 +0530

    Create nnnn.c

---
+++ nnnn.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ int main()
+{
+int a, hour, min, seco;
+    printf("input number in total seconds\n");
+    scanf("%d", &input a);
+    while(input a > 3600){
+    hour = input a /3600;
+    min = input a % 60;
+    seco = min % 60;
+    printf("%dhours %dminutes and %dseconds\n",hour,min,seco);
+    }   
+    min = input a % 60;
+    seco = input a % 60;
+    printf("%dminutes and %dseconds\n",min,sec);
+    }
