9e2041b0681dc661bc15d4c04bb44fd146962066
kumaransabapathy <31533930+kumaransabapathy@users.noreply.github.com>
2018-01-24 16:35:28 +0530

    Create 111222333444555666777888999000.c

---
+++ 111222333444555666777888999000.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+int i,n; 
+char a[70];
+printf("enter the value of n and a");
+scanf("%s%d",&a,&n);
+printf("\n %s%d",a,n);
+for(i=1;i<=n;i++)
+{
+	printf("%s",a);
+	// your code goes her
+}
