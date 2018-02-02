861b312a03670de13e10dd7cd39f90f0577d0ca3
kumaransabapathy <31533930+kumaransabapathy@users.noreply.github.com>
2018-02-02 14:26:00 +0530

    Create saassasss.c

---
+++ saassasss.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a=0,b=1,c,n,count=0;
+printf("enter the number");
+scanf("%d",&n);
+printf("%d",a);
+printf("%d",b);
+  copunt=2;
+  while(count<n)
+  {
+c=a+b;
+    count++;
+printf("%d",c);
+    a=b;
+    b=c;
+}
+return 0;
+}
