fbfb7476ddad061d626a6b136b68c7d0e1887f92
kumaransabapathy <31533930+kumaransabapathy@users.noreply.github.com>
2018-02-07 15:38:06 +0530

    Create fs.c

---
+++ fs.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+  char a[50];
+  int l,i,count=0;
+  printf("Enter the sting:");
+  scanf("%s",a);
+  l=strlen(a);
+  for(i=0;i<l;i++)
+  {
+    if(a[i]=='0'||a[i]=='1')
+    {
+      count++;
+    }
+  }
+   if(count==l)
+   {
+    printf("YES");
+   }
+   else
+   {
+     printf("No");
+}
