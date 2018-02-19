fbf3ce948c061eb26762969b6f173945ab5bc79e
kumaransabapathy <31533930+kumaransabapathy@users.noreply.github.com>
2018-02-19 16:04:29 +0530

    Create qq.c

---
+++ qq.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n1,n2,diff;
+	printf("enter the number1:");
+	scanf("%d",&n1);
+	printf("enter the number2:");
+	scanf("%d",&n2);
+	diff=n1-n2;
+  printf("hjsdfhjdfs")
+  }
