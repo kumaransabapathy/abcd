be9d5c9e0e0cc41673cbeecd25eeaebc59dc8eee
kumaransabapathy <31533930+kumaransabapathy@users.noreply.github.com>
2018-03-07 16:09:54 +0530

    Create dddd1.c

---
+++ dddd1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+ int a,b,mul;
+
+ scanf("%d %d",&a,&b);
+    mul=a*b;
+    if(a==b)
+    printf("yes");
+    else
+    printf("no");
+ 
+  return 0;
+}
+return 0;
+}
+}
+}
