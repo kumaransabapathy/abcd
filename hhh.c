1ab4f1f9ccadc794c65452799fcefc2f8655eb68
kumaransabapathy <31533930+kumaransabapathy@users.noreply.github.com>
2018-03-07 16:16:14 +0530

    Create hhh.c

---
+++ hhh.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int k,o,diff,i;
+    
+    for(i=0;i<3;i++)
+    {
+        scanf("%d %d",&k,&o);
+    diff=abs(k-o);
+    printf("%d\n",diff);
+    }
+    }
+return 0;
+}
+}
+}
